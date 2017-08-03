470a60a62cb8672ce9949fe6b1b0da65b71c5aa4
Kushal Hada <kushaldeveloper@gmail.com>
2017-08-03 15:24:30 -0400

    add a birthday quiz
    wip

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+});
+
+function calculate() {
+  let dateOfBirth = document.getElementById("dateOfBirth");
+  alert(dateOfBirth.val());
+}
+
