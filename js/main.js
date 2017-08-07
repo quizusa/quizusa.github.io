155f6edd219b0f58622c67e4c1bac851f2a9cdb7
Kushal Hada <kushaldeveloper@gmail.com>
2017-08-07 16:56:02 -0400

    add a birthday quiz
    wip

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 function calculate() {
-  let dateOfBirth = document.getElementById("dateOfBirth");
-  alert(dateOfBirth.val());
+  var _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  let dateOfBirth = new Date(document.getElementById("dateOfBirth").value);
+  const currentDate = new Date();
+  const currentDateUTC = Date.UTC(currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate());
+  const dateOfBirthUTC = Date.UTC(dateOfBirth.getFullYear(),
+      dateOfBirth.getMonth(),
+      dateOfBirth.getDate());
+  const age = Math.floor((currentDateUTC - dateOfBirth) / _MS_PER_DAY);
 }
 
